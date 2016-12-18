0489b4f05d0389a9519f63786376aeb6852905f4
howiewang <zyhowie@gmail.com>
2016-12-18 17:58:38 +0800

    add view and url

--- code/posts/views.py
+++ code/posts/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def posts_page(request):
+    return HttpResponse("<h1>sublime python</h1>")
+
+
+def all(request):
+    return HttpResponse("<h1>all list</h1>")
+
+
+def detail(request):
+    return HttpResponse("<h1>sublime detail</h1>")
+
+
+def update(request):
+    return HttpResponse("<h1>update 123</h1>")
+
+
+def delete(request):
+    return HttpResponse("<h1>delete ---</h1>")
---
+++ code/readme.md
@@ -0,0 +1,23 @@
+# view and url
+
+create own view page 
+
+function based view
+classbased  view
+
+
+---Request & Response
+see the url and log for detail
+
+the view handle the request and return the reponse
+
+--- url
+the see what to return.
+Mapping URLs to Views
+
+
+
+---
+## error
+
+- don't forget the , after each url in urlpatterns list!
