1e49b619f39fa15e0995792393583e268445ea96
HowieWang <zyhowie@gmail.com>
2016-12-22 18:41:09 +0800

    modify list page with timesince and truncatechars

--- code/posts/models.py
+++ code/posts/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
     title = models.CharField(max_length=50)  # a post tile is always needed
     content = models.TextField()
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
-    timestamp = models.DateTimeField(auto_now=False, auto_now_add=False, null=True)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
     def __str__(self):
         return self.title
